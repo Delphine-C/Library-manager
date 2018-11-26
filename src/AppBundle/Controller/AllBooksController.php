176a99c3ca97499bb0756a7d8f15c6c3794fd65f
Delphine-C <corneil.delphine@gmail.com>
2018-11-26 19:32:43 +0100

    Get all books from database

---
+++ src/AppBundle/Controller/AllBooksController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Delphine_Corneil
+ * Date: 26/11/2018
+ * Time: 15:03
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AllBooksController extends Controller
+{
+    /**
+     * @Route("/all-books", name="all_books")
+     */
+    public function getBooksAction()
+    {
+        $books =$this->getDoctrine()->getManager()->getRepository('AppBundle:Books')->findAll();
+
+        return $this->render('books/all_books.html.twig', ["books"=>$books]);
+    }
+}

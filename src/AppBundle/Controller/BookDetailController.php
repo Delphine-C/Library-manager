9dc3b3016319af115d5b794e49019ad5bfea113a
Delphine-C <corneil.delphine@gmail.com>
2018-11-27 17:55:12 +0100

    Book detail and book update

---
+++ src/AppBundle/Controller/BookDetailController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Delphine_Corneil
+ * Date: 27/11/2018
+ * Time: 16:40
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Form\BooksType;
+use Symfony\Component\HttpFoundation\Request;
+
+class BookDetailController extends Controller
+{
+    /**
+     * @Route("/book-detail/{id}", name="book_detail")
+     */
+    public function getBookDetailAction(Request $request, $id)
+    {
+        $book = $this->getDoctrine()->getManager()->getRepository('AppBundle:Books')->find($id);
+
+        if(\is_null($book)){
+            throw new NotFoundHttpException(sprintf("This reference doesn't exist.", $id));
+        }
+
+        $form = $this
+            ->createForm(BooksType::class, $book)
+            ->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirect($request->getUri());
+        }
+
+        return $this->render('books/book_detail.html.twig', [
+            "form" => $form->createView(),
+            "book" => $book
+        ]);
+    }
+}
